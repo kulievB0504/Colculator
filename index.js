0ff2f382a14dd98715518ad8cd84c277aea858e3
kulievB0504 <kbekzod2005@gmail.com>
2023-06-21 13:55:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// let guests = "putin biden zelenskiy mirziyoyev trump"
+
+
+// let name = prompt("What is your name Mr. President?")
+
+// if (guests.includes(name)) {
+//     alert("Welcome Mr" + name)
+// } else if (guests.includes(putin)) {
+//     alert('Добро пожаловать' + "." + guests)
+// }
+
+
+
+
+let a = +prompt('a')
+let m = prompt('+ - * /')
+let b = +prompt('b')
+
+if (m === "+") {
+    alert(a + b)
+} else if (m === "-") {
+    alert(a - b)
+} else if (m === "*") {
+    alert(a * b)
+} else if (m === "/") {
+    alert(a / b)
+}
